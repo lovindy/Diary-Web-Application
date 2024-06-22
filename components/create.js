da4c6e44cf145e8863bc9e5dcbe445583e48fb56
lovindy <seangdy085@gmail.com>
2024-06-22 06:54:54 -0700

    style on modal.

--- components/create.js
+++ components/create.js
@@ -13,7 +13,8 @@ function saveCardToLocalStorage(id, title, note, date, category) {
   localStorage.setItem("cards", JSON.stringify(cards));
 }
 
-document.getElementById("addCardBtn").addEventListener("click", () => {
+document.getElementById("addCardBtn").addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent the default form submission
   const title = document.getElementById("title").value;
   const note = document.getElementById("note").value;
   const category = document.getElementById("category").value;
@@ -23,7 +24,7 @@ document.getElementById("addCardBtn").addEventListener("click", () => {
     const date = formatDate(now);
     const id = Date.now().toString();
     saveCardToLocalStorage(id, title, note, date, category);
-    window.location.href = "../pages/list.html";
+    window.location.href = "/pages/list.html";
   } else {
     alert("Please fill out all fields.");
   }
