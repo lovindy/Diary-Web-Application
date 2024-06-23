0769e9fd35d4f0316dead28160df5b7c14f56fce
lovindy <seangdy085@gmail.com>
2024-06-22 23:48:29 -0700

    working navigation menu

--- components/nav.js
+++ components/nav.js
@@ -17,7 +17,7 @@ function createNavHTML() {
       <ul class="nav-links">
   `;
   links.forEach((link) => {
-    navHTML += `<li class="nav-link"> <a href="${link.href}" class="underline-animation">${link.text}</a> </li>`;
+    navHTML += `<li class="nav-link"><a href="${link.href}" class="underline-animation">${link.text}</a></li>`;
   });
 
   // Add the login and signup buttons
@@ -30,9 +30,10 @@ function createNavHTML() {
         <button class="button button-primary">Login</button>
         <button class="button button-secondary">Sign Up</button>
       </div>
-      <div class="menu-icon">
-        <img src="/assets/icons/menu-icon.svg" alt="Diary Logo" />
+      <div class="menu-icon" onclick="toggleMenu()">
+        <img src="/assets/icons/menu-icon.svg" alt="Menu Icon" />
       </div>
+      <div class="close-icon" onclick="toggleMenu()">x</div>
     </nav>
   `;
   return navHTML;
@@ -47,3 +48,20 @@ class NavComponent extends HTMLElement {
 
 // Register the custom element
 customElements.define("nav-component", NavComponent);
+
+// Add the toggleMenu function
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  const navButtons = document.querySelector(".nav-buttons");
+  if (navLinks.classList.contains("open")) {
+    navLinks.classList.remove("open");
+    navLinks.classList.add("closed");
+    navButtons.classList.remove("open");
+    navButtons.classList.add("closed");
+  } else {
+    navLinks.classList.remove("closed");
+    navLinks.classList.add("open");
+    navButtons.classList.remove("closed");
+    navButtons.classList.add("open");
+  }
+}
